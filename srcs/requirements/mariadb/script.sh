9062dad5540bf93a41383f5a4575fdc3032f800d
shmorish <110565242+shmorish@users.noreply.github.com>
2024-12-22 03:08:52 +0900

    fix mariadb script

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-envsubst < /tmp/init.sql > /etc/mysql/init.sql
+create_database() {
+    # check if /etc/mysql/init.sql already exists
+    if [ -f /etc/mysql/init.sql ]; then
+        echo "init.sql already exists"
+    else
+        echo "Generating init.sql"
+        envsubst < /tmp/init.sql > /etc/mysql/init.sql
+    fi
+}
 
-mysql_install_db
-mysqld
+
+install() {
+    # check if mysql is already installed
+    if [ -d /var/lib/mysql/mysql ]; then
+        echo "mysql already installed"
+    else
+        mysql_install_db --user=root --datadir=/var/lib/mysql
+    fi
+}
+
+main() {
+    create_database
+    install
+    mysqld
+}
+
+main
